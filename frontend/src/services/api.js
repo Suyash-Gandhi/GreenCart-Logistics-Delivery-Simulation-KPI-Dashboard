f4075ba02d67a6f298d89e8202597c035427e4ec
Suyash-Gandhi <162720228+Suyash-Gandhi@users.noreply.github.com>
2025-08-13 12:32:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL:"https://greencart-logistics-delivery-simulation-pk8w.onrender.com/api",
+  baseURL:"https://greencart-logistics-delivery-simulation-pk8w.onrender.com",
   headers: { 'Content-Type': 'application/json' }
 });
 
